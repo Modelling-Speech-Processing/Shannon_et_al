b62e8f2826a205fde0d3871d42e1fe577ec8593e
Andrew Shannon <drewan97@gmail.com>
2026-02-13 17:22:14 +0000

    Plotting peri phoneme onset time window view of fast nmm response.

---
+++ Plot_phase_reset_ripples.jl
@@ -0,0 +1,94 @@
+using Pkg; Pkg.activate("$(pwd())")
+using NGNMM_NSP_paper_code
+using Statistics
+using ComponentArrays, OrdinaryDiffEq, Plots, Parameters, JLD2
+
+
+condition="b" #which phoneme condition to use for the drive
+idx=1 #which test index to use
+noisestimratio=0.0 #which noise to stimulus ratio to use
+noise_filename="NoiseSequences60.csv" #which noise file to use
+
+global NGNMM_NSP_paper_code.interpolators_global = jldopen("./Phoneme_Drives/drive_interpolators_$(condition)_test_$(idx)_NSR_$(noisestimratio)_$(split(noise_filename,".")[1]).jld2","r")["drives"]
+
+#fast NGNMM model's response to each phoneme:
+# set parameters
+α=1/0.035 #1/a is 30ms
+k=0.105
+C = 0.033#capacitance in Farads
+# C = 0.066#capacitance in Farads
+vsyn=-10.0
+η_0=21.5
+# η_0=1.5
+Δ=0.5
+# Δ=0.25
+α_D=1/0.0056
+Π=15.0
+#Drive 
+DAR=12.2
+phoneme_sampling_rate=44100
+drive_amplitude=η_0*DAR
+p=ComponentArray(α=α,k=k,C=C,vsyn=vsyn,Δ=Δ, η_0=η_0,α_D=α_D,sampling_rate=phoneme_sampling_rate, drive_amplitude=drive_amplitude,noise_selector=1,noise_case_reference=1) 
+u0=ComponentArray(g_dot=0.0,g=0.82,Z=0.0+im*0.0, A_dot=0.0, A=0.0)
+time_range=(0.0,10.0)
+saveat=0.0001   
+
+prob_func=vary_noise_and_initial_conditions_NGNMM
+
+results=Ensemble_NoisyPhoneme(prob_func,time_range,p,u0,20,saveat)
+
+#find the start of each phoneme in the 5 second stimulus window (5.0, 10.0)
+#the stimulus is at ~4Hz with some noise, so maybe try for now to just take 5, 5.25 etc.. 
+#then plot the mean response in a 200ms window after these times, across all 20 runs and all ~20 phonemes in each run.
+plot(abs.(results[2][3,:]),dpi=300,xlims=(25000,30000))
+stim_times=collect(range(5.0,9.0,step=0.25))
+response_window=0.25
+response_sr=Int64(1/saveat) 
+response_window_samples=Int64(response_window*response_sr) 
+
+responses=Vector{Vector{Float64}}(undef,340)
+for i in 1:20
+    for j in 1:17
+        responses[(i-1)*17+j]=abs.(results[i][3,Int64((stim_times[j]-response_window)*response_sr):Int64(stim_times[j]*response_sr+response_window_samples)])
+    end
+end
+
+response_times=range(-0.25,0.25,step=1/response_sr)
+plot(response_times,responses[5],label="single trial")
+plot!(response_times,mean(responses),label="mean response")
+stimulus_time_points=range(-0.25,0.25,step=1/44100)
+plot!(stimulus_time_points,interpolators_global[1]((stim_times[2]-0.25)*44100:stim_times[3]*44100),label="example drive")
+plot!(legend=:bottomright)
+plot!(ylabel=L"\textrm{firing~rate}", xlabel=L"\textrm{peri\operatorname{-}onset~time~(s)}")
+
+#size and save it:
+one_column_size=figure_size_tuple(1, aspect_ratio=1.7)
+plot!(size=one_column_size,legend=:outertopright,dpi=300,xticks=([-0.2,0,0.2],[-0.2,0,0.2]))
+savefig("phase_reset_ripples.pdf")
+
+
+interpolators_global[1]((stim_times[1]-0.25)*44100:stim_times[2]*44100)
+for i in 1:20
+    for j in 1:17
+        println(((i-1)*17)+j)
+    end
+end
+
+
+#check state at 5 seconds of the response to make sure it has been randomised...
+init_conds=Vector{ComplexF64}(undef,20)
+for i in 1:20
+    init_conds[i]=results[i][3,Int64(5.0*response_sr)]
+    # init_conds[i]=results[i][3,1]#,Int64(5.0*response_sr)]
+end
+scatter!(init_conds,xlims=(-1,1),ylims=(-1,1))
+
+plot(abs.(results[1][3,:]))
+p=plot()
+for i in 1:20
+    plot!(p,response_times,abs.(results[i][3,Int64((stim_times[1]-response_window)*response_sr):Int64(stim_times[1]*response_sr+response_window_samples)]))
+end
+plot(p)
+plot!(dpi=300,legend=nothing, xlabel=L"\textrm{peri\operatorname{-}onset~time~(s)}",ylabel=L"\textrm{firing~rate}")
+plot!(size=one_column_size)
+savefig("peri_onset_time_responses.pdf")
